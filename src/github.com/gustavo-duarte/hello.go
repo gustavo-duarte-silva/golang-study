de56669f2e02bbe60918374f877f924a3a5ee4cf
Gustavo Duarte <gustavo.duarte@blueshift.com.br>
2024-11-14 15:54:09 -0300

    Update 1.4

--- src/github.com/gustavo-duarte/hello.go
+++ src/github.com/gustavo-duarte/hello.go
@@ -4,14 +4,23 @@ import "fmt"
 
 //Evita de criar variaveis a todo momento!
 const prefixHello = "Hello, "
+const prefixHelloSpanish = "Hola, "
+const prefixHelloFrances = "Bonjour, "
 
-func Hello(nome string) string {
+func Hello(nome string, idioma string) string {
+	prefixo := prefixHello
 	if nome == "" {
 		nome = "World"
 	}
-	return prefixHello + nome
+	switch idioma {
+	case "frances":
+		prefixo = prefixHelloFrances
+	case "spanish":
+		prefixo = prefixHelloSpanish
+	}
+	return prefixo + nome
 }
 
 func main() {
-	fmt.Println(Hello("mundo"))
+	fmt.Println(Hello("Django", ""))
 }
--- src/github.com/gustavo-duarte/hello_test.go
+++ src/github.com/gustavo-duarte/hello_test.go
@@ -8,18 +8,30 @@ func TestHello(t *testing.T) {
 	verifyMessage := func(t *testing.T, results string, target string) {
 		t.Helper()
 		if results != target {
-			t.Errorf("Results: '%s'Target: '%s'", results, target)
+			t.Errorf("Results: '%s' | Target: '%s'", results, target)
 		}
 	}
 	t.Run("Diz olá para as pessoas", func(t *testing.T) {
-		results := Hello("Chris")
+		results := Hello("Chris", "")
 		target := "Hello, Chris"
 		verifyMessage(t, results, target)
 	})
 
 	t.Run("Diz 'Olá, mundo' quando for uma string vazia", func(t *testing.T) {
-		results := Hello("")
+		results := Hello("", "")
 		target := "Hello, World"
+		verifyMessage(t, results, target)
+	})
+
+	t.Run("Em espanhol", func(t *testing.T) {
+		results := Hello("Elodie", "spanish")
+		target := "Hola, Elodie"
+		verifyMessage(t, results, target)
+	})
+
+	t.Run("Frances", func(t *testing.T) {
+		results := Hello("Finn", "frances")
+		target := "Bonjour, Finn"
 		verifyMessage(t, results, target)
 	})
 }
